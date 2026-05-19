ca21009a4165271f96e696593fedfbcd06b3bed6
calciferDeLioncourt <62509211+calciferDeLioncourt@users.noreply.github.com>
2026-05-19 09:56:56 -0600

    path_data

--- src/proyecto_final/infrastructure/database/session.py
+++ src/proyecto_final/infrastructure/database/session.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+
+Path("data").mkdir(exist_ok=True)
 
 DATABASE_URL = "sqlite:///./data/orders.db"
 
