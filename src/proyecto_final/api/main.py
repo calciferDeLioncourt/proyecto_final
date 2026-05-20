9e628e33046c8a99a1662df5c9546b7bd86c5c92
calciferDeLioncourt <62509211+calciferDeLioncourt@users.noreply.github.com>
2026-05-20 09:47:17 -0600

    deploy-heroku

--- src/proyecto_final/api/main.py
+++ src/proyecto_final/api/main.py
@@ -2,17 +2,22 @@
 from fastapi import FastAPI
 
 from proyecto_final.api.routers.auth_router import router as auth_router
-from proyecto_final.api.routers.health_router import (
-    router as health_router,
+from proyecto_final.api.routers.health_router import router as health_router
+from proyecto_final.api.routers.orders_router import router as orders_router
+from proyecto_final.infrastructure.database.models.order_item_model import (  # noqa E402
+    OrderItemModel,
 )
-from proyecto_final.api.routers.orders_router import (
-    router as orders_router,
+from proyecto_final.infrastructure.database.models.order_model import (  # noqa E402
+    OrderModel,
 )
+from proyecto_final.infrastructure.database.session import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(
     title="APIs Web FastAPI",
     version="1.0.0",
-    description="Test Intermediate Level API",
+    description="Poyecto final",
 )
 
 # Registro de routers
